a4171c89538283ffda7779c08cd17deb3e828ffb
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-05-17 17:53:48 +0300

    Add order constructor and global variables to be used by the constructor

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-
+function order(size, crust, quantity, deliver, location) {
+  this.size = size;
+  this.crust = crust;
+  this.quantity = quantity;
+  this.deliver = deliver;
+  this.location = location;
+  this.toppings = [];
+  }
+var singlePizzaPrice;
+var deliveryPrice = 300;
